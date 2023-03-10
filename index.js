b32d508fcd53eb23522b195571b28f42f35031eb
toocels <toocels@gmail.com>
2023-03-10 15:18:08 +0530

    im done

--- index.js
+++ index.js
@@ -13,27 +13,3 @@ app.get('/', (req, res) => {
 app.listen(process.env.PORT || 3000);
 
 module.exports = app;
-
-
-// // Import packages
-// const express = require("express");
-// const home = require("./routes/home");
-
-// // Middlewares
-// const app = express();
-// app.use(express.json());
-
-// // Routes
-// app.use(express.static('public'))
-// app.use("/home", home);
-
-// // index.js
-// app.get('/', (req, res) => {
-//   res.sendFile('index.html', {root: path.join(__dirname, 'public')});
-// })
-
-// // connection
-// const port = process.env.PORT || 80;
-// app.listen(port, () => console.log('Listening to port:', port));
-
-// module.exports = app;
