e3eeeec95708d30ef4017deafbd39c107fa0fce6
toocels <toocels@gmail.com>
2023-03-10 15:14:37 +0530

    minimap from github/express-to-vercel

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-// Import packages
-const express = require("express");
-const home = require("./routes/home");
-
-// Middlewares
+const express = require('express');
 const app = express();
-app.use(express.json());
+const path = require('path');
 
-// Routes
 app.use(express.static('public'))
-app.use("/home", home);
 
-// index.js
 app.get('/', (req, res) => {
-  res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
 })
 
-// connection
-const port = process.env.PORT || 80;
-app.listen(port, () => console.log('Listening to port:', port));
+console.log("hehehehehe")
+
+app.listen(process.env.PORT || 3000);
 
 module.exports = app;
+
+
+// // Import packages
+// const express = require("express");
+// const home = require("./routes/home");
+
+// // Middlewares
+// const app = express();
+// app.use(express.json());
+
+// // Routes
+// app.use(express.static('public'))
+// app.use("/home", home);
+
+// // index.js
+// app.get('/', (req, res) => {
+//   res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+// })
+
+// // connection
+// const port = process.env.PORT || 80;
+// app.listen(port, () => console.log('Listening to port:', port));
+
+// module.exports = app;
